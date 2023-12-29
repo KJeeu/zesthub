e9b6e84246ccd568b7332e088b5ae0537ad0e904
JiHo Kim <kr36510@naver.com>
2023-12-29 14:31:08 +0900

    QueryClient 설정: 리렌더링, 창 포커스시 쿼리 실행되지 않도록 함

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App.tsx";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			retry: 1,
+		},
+	},
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
