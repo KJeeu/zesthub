0f27cb7fee8c96cea30135ca5653f64510391702
JiHo Kim <kr36510@naver.com>
2024-01-12 03:21:29 +0900

    재료 선택 버튼 색상 랜덤

---
+++ client/src/constants/randomColor.ts
@@ -0,0 +1,9 @@
+export const randomColor = [
+	"var(--primary-1)",
+	"var(--primary-2)",
+	"var(--primary-3)",
+	"var(--primary-4)",
+	"var(--primary-5)",
+	"var(--primary-6)",
+	"var(--primary-7)",
+] as const;
