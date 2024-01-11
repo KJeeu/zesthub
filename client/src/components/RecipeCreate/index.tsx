71957eb54328b437bf20cf56c6096826da6cb6cc
JiHo Kim <kr36510@naver.com>
2024-01-12 03:19:11 +0900

    레시피 만들기 버튼

---
+++ client/src/components/RecipeCreate/index.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import IconButton from "../IconButton";
+import FoodSelectList from "./FoodSelectList";
+
+import { useFoodSelectList } from "@/hooks/useFoodSelectList";
+import { useUserFoodSelectStore } from "@/store";
+
+const RecipeCreate = () => {
+	const { closeFoodInfo } = useUserFoodSelectStore();
+	const { isOpen, openFoodSelectList, closeFoodSelectList } =
+		useFoodSelectList();
+
+	const handleRecipeCreate = () => {
+		closeFoodInfo();
+		openFoodSelectList();
+	};
+
+	return (
+		<Wrapper>
+			{!isOpen && (
+				<Container>
+					<IconButton
+						type="recipeCreate"
+						onClick={handleRecipeCreate}
+						padding="1.2rem"
+					/>
+				</Container>
+			)}
+			<FoodSelectList isOpen={isOpen} close={closeFoodSelectList} />
+		</Wrapper>
+	);
+};
+
+export default RecipeCreate;
+
+const Wrapper = styled.section`
+	display: flex;
+	justify-content: center;
+	width: 100%;
+	height: 50%;
+`;
+
+const Container = styled.section`
+	width: 80%;
+`;
