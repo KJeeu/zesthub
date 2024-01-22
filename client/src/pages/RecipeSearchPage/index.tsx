27ba920188a14f48f6f308d7458061bc72f02d66
JiHo Kim <kr36510@naver.com>
2024-01-22 23:24:29 +0900

    레시피 페이지 생성

--- client/src/pages/RecipeSearchPage/index.tsx
+++ client/src/pages/RecipeSearchPage/index.tsx
@@ -1,9 +1,20 @@
 import styled from "styled-components";
+import { useQuery } from "@tanstack/react-query";
 import { Link, useParams } from "react-router-dom";
 import { FONT_SIZE } from "@/styles/common";
+import { getRecipe } from "@/api/recipe";
+import SearchResultBox from "@/components/SearchResultBox";
+import type { RecipeListData } from "@/types/api.types";
 
 const RecipeSearchPage = () => {
 	const { foodCart } = useParams();
+
+	const { data: RecipeList } = useQuery({
+		queryKey: ["recipe", foodCart],
+		queryFn: () => getRecipe(foodCart!),
+		staleTime: Infinity,
+		gcTime: Infinity,
+	});
 
 	return (
 		<Container>
@@ -14,8 +25,15 @@ const RecipeSearchPage = () => {
 				<SubTitle>재료 : {foodCart}</SubTitle>
 			</Banner>
 			<ResultSection>
-				<ResultCount>20개의 매장</ResultCount>
-				{foodCart}
+				{!RecipeList && <ResultCount>해당하는 레시피가 없습니다.</ResultCount>}
+				{RecipeList && (
+					<>
+						<ResultCount>{RecipeList.recipeListCount}개의 레시피</ResultCount>
+						{RecipeList?.recipeList.map((recipe: RecipeListData) => (
+							<SearchResultBox recipe={recipe} />
+						))}
+					</>
+				)}
 			</ResultSection>
 		</Container>
 	);
@@ -27,9 +45,11 @@ const Container = styled.div`
 	display: flex;
 	flex-direction: column;
 	gap: 2.4rem;
+
 	width: 100%;
 	max-width: 1240px;
 	min-height: 100vh;
+
 	margin: 0 auto;
 `;
 
@@ -52,18 +72,22 @@ const ResultSection = styled.div`
 	display: flex;
 	flex-direction: column;
 	gap: 2.4rem;
+
 	width: 100%;
+
 	padding: 0 1.2rem;
 `;
 
 const Title = styled.h5`
 	margin: 1.6rem 1.2rem;
 	font-weight: bold;
+
 	color: white;
 `;
 
 const SubTitle = styled.span`
 	margin: 1.6rem 1.2rem;
 	font-size: ${FONT_SIZE.md};
+
 	color: white;
 `;
