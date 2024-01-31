869432295a9f7eb1331980678c80d8cdad0578a1
JiHo Kim <kr36510@naver.com>
2024-01-31 23:58:08 +0900

    이미지와 식재료 API 연동하라

--- client/src/api/refrigerator.ts
+++ client/src/api/refrigerator.ts
@@ -1,6 +1,14 @@
 import { db, app } from "@/firebase";
-import { collection, getDocs, query, where } from "firebase/firestore";
-import { getStorage, ref, getDownloadURL } from "firebase/storage";
+import {
+	collection,
+	getDocs,
+	query,
+	where,
+	deleteDoc,
+	doc,
+	updateDoc,
+} from "firebase/firestore";
+import { getStorage, ref, deleteObject, uploadBytes } from "firebase/storage";
 
 import type { CategoryData, FoodCartData } from "@/types/api.types";
 
@@ -30,7 +38,7 @@ export const getFoodCart = async (
 	const data = response.docs.map((foodCart) => {
 		const data = foodCart.data();
 		const foodCartData: FoodCartData = {
-			buyData: data["buyData"],
+			buyDate: data["buyDate"],
 			category: data["category"],
 			count: data["count"],
 			expiryDate: data["expiryDate"],
@@ -45,8 +53,34 @@ export const getFoodCart = async (
 	return data;
 };
 
-export const getFoodImage = async (image: string) => {
+export const getFoodImage = (image: string) => {
+	return `https://firebasestorage.googleapis.com/v0/b/my-refrigerator-8ce31.appspot.com/o/cart%2F${image}?alt=media`;
+};
+
+export const deleteFood = async (id: string) => {
+	deleteDoc(doc(db, "cart", id));
+};
+
+export const deleteFoodImage = async (imageName: string) => {
 	const storage = getStorage(app);
-	const imageRef = ref(storage, `cart/${image}`);
-	return await getDownloadURL(imageRef);
+	deleteObject(ref(storage, `cart/${imageName}`));
+};
+
+export const uploadImage = async (currentImage: File) => {
+	const name = currentImage.name;
+	const storage = getStorage(app);
+	const image = ref(storage, `cart/${name}`);
+	await uploadBytes(image, currentImage);
+};
+
+export const updateFood = async (item: FoodCartData) => {
+	await updateDoc(doc(db, "cart", item.id), {
+		category: item.category,
+		name: item.name,
+		buyDate: item.buyDate,
+		expiryDate: item.expiryDate,
+		count: item.count,
+		place: item.place,
+		image: item.image,
+	});
 };
